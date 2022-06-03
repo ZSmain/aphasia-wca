16f64e8e009a94364754c9137cbeca29a582cbe5
ismail <ishmael.zouaoui@gmail.com>
2022-06-03 09:24:48 +0100

    Adding the MEDIA_URL and MEDIA_ROOT path to the url patterns to correctly retrieve the images from the media folder.

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,4 +1,7 @@
-from django.urls import URLPattern, path
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -6,3 +9,6 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     # path('quiz/<int:quiz_id>/', views.quiz, name='quiz'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
