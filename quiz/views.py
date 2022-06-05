326a348a42ccd79298c8780bf05ed1954ea6c087
ismail <ishmael.zouaoui@gmail.com>
2022-06-06 00:25:20 +0100

    Adding the View that will render the Quiz Starting page.

--- quiz/views.py
+++ quiz/views.py
@@ -2,11 +2,12 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import Quiz
+from .models import Answer, Question, Quiz, Take, UserAnswer
 
 
 @login_required
 def home(request):
+    # get all quizes.
     quizes = Quiz.objects.all()
     
     context = {'quizes': quizes}
@@ -16,4 +17,16 @@ def home(request):
 @login_required
 def dashboard(request):
     return render(request, 'quiz/dashboard.html')
+
+@login_required
+def quiz(request, quiz_id):
+    
+    quiz = Quiz.objects.filter(id=quiz_id).first()
+    
+    # get all questions ids of this quiz.
+    questions_ids = Question.objects.filter(quiz=quiz).values_list('id', flat=True)
+    
+    context = {'quiz': quiz, 'questions_ids': questions_ids}
+    
+    return render(request, 'quiz/quiz.html', context)
 
