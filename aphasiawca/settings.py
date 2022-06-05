69b88d403e4c88a802d1fed6b5f170e6bd0a8dd4
ismail <ishmael.zouaoui@gmail.com>
2022-06-06 00:03:57 +0100

    Removing the STATIC_ROOT path from the STATICFILES_DIRS.

--- aphasiawca/settings.py
+++ aphasiawca/settings.py
@@ -115,7 +115,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "quiz/static"),
 ]
 
