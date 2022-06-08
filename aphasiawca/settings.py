de351137f631491204cd43d479fd4b40b5528a38
ismail <ishmael.zouaoui@gmail.com>
2022-06-08 23:48:14 +0100

    Adding dj_database_url to update the Heroku database using the local database.

--- aphasiawca/settings.py
+++ aphasiawca/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +83,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
